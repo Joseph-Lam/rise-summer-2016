8a1e71cec4caa2411c4d3a0738ed81f610db18cc
teeppope <terraepop@gmail.com>
2016-09-08 11:06:31 -0700

    add basic structure for contact process

---
+++ themes/rise-legal/template-parts/content-contact-process.php
@@ -0,0 +1,16 @@
+<div>
+	<div>
+		<img src="">
+		<p>Fill Form</p>
+	</div>
+	<img src="">
+	<div>
+		<img src="">
+		<p>Submit Form</p>
+	</div>
+	<img src="">
+	<div>
+		<img src="">
+		<p>We will contact you</p>
+	</div>
+</div>
