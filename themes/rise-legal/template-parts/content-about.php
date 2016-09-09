218dbbe3726734ffd633d12091c42bba19da62a6
teeppope <terraepop@gmail.com>
2016-09-09 09:12:06 -0700

    remove pre line height

---
+++ themes/rise-legal/template-parts/content-about.php
@@ -0,0 +1,25 @@
+<section class="about-content">
+	<?php the_title( '<h1 class="front-title">', '</h1>' ); ?>
+	<?php the_content(); ?>
+</section>
+<section class="our-people">
+	<div class="title-banner donate-banner flex-center">
+		<img src="<?php echo get_template_directory_uri() ."/assets/logos/rise-logo-white.svg"?>">	
+	</div>
+	
+	<div>
+		<ul class="our-staff">
+			<li></li>
+		</ul>
+		<ul class="our-board">
+			<li></li>
+		</ul>
+		<ul class="our-partners">
+			<li></li>
+		</ul>
+		<ul class="our-students">
+			<li></li>
+		</ul>
+	</div>
+
+</section>
