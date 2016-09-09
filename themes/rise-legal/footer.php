a7051b9f1c28c9e2ffd314fd113a5a7cb2b922cc
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-09 01:02:28 -0700

    styled footer for mobile view

--- themes/rise-legal/footer.php
+++ themes/rise-legal/footer.php
@@ -10,26 +10,33 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="footer-content">
-			<div class="contact-info">	
-				<p>
-					<i class="fa fa-phone" aria-hidden="true" style="color: white"></i>
-					<a href="778-456-7891">778-456-7891</a>
-				</p>
-				<p>
-					<i class="fa fa-envelope" aria-hidden="true" style="color: white"></i>
-					<a href="info@womenslegalcentre.ca">info@womenslegalcentre.ca</a>
-				</p>
-				<p>
-					<i class="fa fa-map-marker" aria-hidden="true" style="color: white"></i>
-					<span><br>"#201-456 West Broadway" <br> "Vancouver, BC V57 1R3</span>
-				</p>
-				<p>
-					<i class="fa fa-facebook-official" aria-hidden="true"></i>
-					<i class="fa fa-twitter" aria-hidden="true"></i>
-				</p>
-			</div>	
+	<div class="footer-content">
+		<div class="contact-info">	
+			<p>
+				<i class="fa fa-phone" aria-hidden="true" style="color: white"></i>
+				<a href="778-456-7891">(604)-451-7447</a>
+			</p>			
+			<p>
+				<i class="fa fa-envelope" aria-hidden="true" style="color: white"></i>
+				<a href="info@womenslegalcentre.ca">info@womenslegalcentre.ca</a>
+			</p>
 		</div>
+		<div class="rise-location">
+			<p>
+				<i class="fa fa-map-marker" aria-hidden="true" style="color: white"></i>
+				<span><br>#201-456 West Broadway <br> Vancouver, BC V57 1R3</span>
+			</p>
+		</div>
+		<div class="footer-media">
+			<p>
+				<a href="facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				
+				<a href="twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></i></a>
+				
+			</p>	
+		</div>		
+	</div>	
+</div>
 </footer>
 
 </div><!-- #page -->
