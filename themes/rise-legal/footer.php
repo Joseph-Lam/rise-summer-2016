d9b2a7ae46b8f03b510ec3e50ca7220ff2a8b2e6
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-09 10:08:55 -0700

    fixed merge conflicts on footer

--- themes/rise-legal/footer.php
+++ themes/rise-legal/footer.php
@@ -9,34 +9,34 @@
 
 </div><!-- #content -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-	<div class="footer-content">
-		<div class="contact-info">	
-			<p>
-				<i class="fa fa-phone" aria-hidden="true" style="color: white"></i>
-				<a href="778-456-7891">(604)-451-7447</a>
-			</p>			
-			<p>
-				<i class="fa fa-envelope" aria-hidden="true" style="color: white"></i>
-				<a href="info@womenslegalcentre.ca">info@womenslegalcentre.ca</a>
-			</p>
-		</div>
-		<div class="rise-location">
-			<p>
-				<i class="fa fa-map-marker" aria-hidden="true" style="color: white"></i>
-				<span><br>#201-456 West Broadway <br> Vancouver, BC V57 1R3</span>
-			</p>
-		</div>
-		<div class="footer-media">
-			<p>
-				<a href="facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-				
-				<a href="twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></i></a>
-				
-			</p>	
-		</div>		
-	</div>	
-</div>
+<footer id="colophon" class="site-footer footer-content" role="contentinfo">
+	<div class="contact-info">	
+		<p>
+			<i class="fa fa-phone" aria-hidden="true" style="color: white"></i>
+			<a href="778-456-7891">(604)-451-7447</a>
+		</p>			
+		<p>
+			<i class="fa fa-envelope" aria-hidden="true" style="color: white"></i>
+			<a href="info@womenslegalcentre.ca">info@womenslegalcentre.ca</a>
+		</p>
+	</div>
+	<div class="rise-location">
+		<p>
+			<i class="fa fa-map-marker" aria-hidden="true" style="color: white"></i>
+			<span></br>#201-456 West Broadway </br> Vancouver, BC V57 1R3</span>
+		</p>
+	</div>
+	<div class="footer-media">
+		<h3>
+			Connect with us
+		</h3>	
+			<a href="facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+
+			<a href="twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></i></a>
+
+	</div>		
+</div>	
+
 </footer>
 
 </div><!-- #page -->
