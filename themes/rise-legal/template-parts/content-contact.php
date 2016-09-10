d0f533ea15b1a7aa6eb39db209c56cb1f2846eb2
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-10 16:30:58 -0700

    added content and icons to Contact Us page

--- themes/rise-legal/template-parts/content-contact.php
+++ themes/rise-legal/template-parts/content-contact.php
@@ -7,29 +7,37 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" class="" <?php post_class(); ?>>
 	<header class="entry-header">
+		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-contactus.svg"?>">
+
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+			'after'  => '</div>',
 			) );
-		?>
+			?>
+
+			<button>Get Directions</button>
+
+			<div class="transit-information">
+				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-transit.svg"?>">
+				<?php echo CFS()->get( 'transit_information' ); ?>
+			</div>
+			<div class="parking-information">
+				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-noparking.svg"?>">
+				<?php echo CFS()->get( 'parking_information' ); ?>
+			</div>
+			<div class="wait-list-information">
+			<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-hourglass.svg"?>">
+				<?php echo CFS()->get( 'wait_list_information' ); ?>
+			</div>
 
 
-
-		<?php echo CFS()->get( 'transit_information' ); ?>
-		<?php echo CFS()->get( 'parking_information' ); ?>	
-		
-
-		<?php echo CFS()->get( 'wait_list_information' ); ?>
-
-
-
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+		</div><!-- .entry-content -->
+	</article><!-- #post-## -->
