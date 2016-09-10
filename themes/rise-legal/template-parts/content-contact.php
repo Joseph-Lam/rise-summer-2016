c59e088cc907662ca5f7c4c0ae35a6ba0dab8d87
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-10 16:50:52 -0700

    fixed maped into content-contact.php and made it responsive

--- themes/rise-legal/template-parts/content-contact.php
+++ themes/rise-legal/template-parts/content-contact.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" class="flex-center contact-container" <?php post_class(); ?>>
 	<header class="entry-header">
 		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-contactus.svg"?>">
 
@@ -22,6 +22,9 @@
 			'after'  => '</div>',
 			) );
 			?>
+			<div class="responsive-map">
+			<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.7115811928634!2d-123.11630378412012!3d49.26291238009192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673dd81bd8ebf%3A0x2c3f66039d0db41c!2sRise+Women's+Legal+Centre!5e0!3m2!1sen!2sca!4v1473549430486" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+			</div>
 
 			<button>Get Directions</button>
 
