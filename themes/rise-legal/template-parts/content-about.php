4452bc5abf50392274a1263f2330bc212ba18d68
teeppope <terraepop@gmail.com>
2016-09-10 11:17:59 -0700

    trying to remove weird generated html elements in the loop

--- themes/rise-legal/template-parts/content-about.php
+++ themes/rise-legal/template-parts/content-about.php
@@ -2,6 +2,7 @@
 	<?php the_title( '<h1 class="front-title">', '</h1>' ); ?>
 	<?php the_content(); ?>
 </section>
+
 <section class="our-people">
 
 	<div class="title-banner donate-banner flex-center">
@@ -9,7 +10,6 @@
 	</div>
 	
 	<div>
-		
 		<ul class="our-staff">
 			<?php
 				$args = array(
@@ -33,45 +33,32 @@
 
 					<?php if ( has_post_thumbnail() ) : ?>
 						<div>
-						
 							<?php the_post_thumbnail( 'original' ); ?>
 						</div>
 					<?php endif; ?>
-
-					<?php if( CFS()->get('position_title')):?>
-						<?php echo CFS()->get('position_title')?>
-					<?php endif; ?>
-
-					<?php if( CFS()->get('employed_at')):?>
-							<?php echo CFS()->get('employed_at')?>
-					<?php endif; ?>
-					
+			
+					<?php echo CFS()->get('position_title')?>
+	
+					<?php echo CFS()->get('employed_at')?>
+				
 					<p><?php echo CFS()->get('biography')?></p>
 				</li>
 			<?php endforeach; wp_reset_postdata(); ?>
 
 		</ul>
+	</div>
 
-	<!-- Need to get our board taxonomy -->
-	<!-- need to get taxonomy description if there is one -->
 	<!-- <ul class="our-board"> -->
-	<!-- Need to loop through people posts tagged with the our board taxonomy and show -->
 		<!-- 	<li></li>
 	</ul> -->
 
-	<!-- Need to get our partners taxonomy -->
-	<!-- need to get taxonomy description if there is one -->
 	<!-- <ul class="our-partners"> -->
-	<!-- Need to loop through people posts tagged with the our partners taxonomy and show including styles for images-->
 		<!-- 	<li></li>
 	</ul> -->
-
-	<!-- Need to get our students taxonomy -->
 
 	<!-- <ul class="our-students"> -->
-	<!-- Need to loop through people posts tagged with the our students taxonomy and show -->
 		<!-- 	<li></li>
 	</ul> -->
-</div>
+
 
 </section>
