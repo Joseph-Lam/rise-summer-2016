d561c257f791736d7f5905c99b2c4e194d64b9bd
teeppope <terraepop@gmail.com>
2016-09-11 10:46:30 -0700

    add desktop section nav structure

--- themes/rise-legal/template-parts/content-about.php
+++ themes/rise-legal/template-parts/content-about.php
@@ -6,7 +6,32 @@
 <section class="our-people">
 
 	<div class="title-banner donate-banner flex-center">
+	<!-- display logo on mobile -->
 		<img src="<?php echo get_template_directory_uri() ."/assets/logos/rise-logo-white.svg"?>" class="rise-about-logo">	
+	<!-- display this nav on desktop -->
+		<nav class="about-people">
+			<ul class="about-people-nav">
+				<?php 
+				$categories = array(
+					'taxonomy' => 'people_group', 
+					'hide_empty' => true,
+					);
+
+				$terms = get_terms( $categories );
+				?>
+				
+				<?php foreach ( $terms as $term ) :  ?>
+					<li class="indvid-people-group"> 
+						<a href="#<?php echo $term->slug; ?>"> 
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$term->slug.".svg"?>">
+							<p><?php echo $term->name ?></p>
+						</a>
+					</li>
+
+				<?php endforeach; ?>
+			</ul>
+		</nav>
+
 	</div>
 	
 	<div>
@@ -20,7 +45,7 @@
 
 			$terms = get_terms( $categories );
 			?>
-		<div class="group-title">	
+		<div class="group-title" >	
 			<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-staff.svg"?>">
 			<?php foreach ( $terms as $term ) :  ?>
 				<h3><?php echo $term->name ?></h3>
@@ -28,7 +53,7 @@
 			<?php endforeach; ?>
 		</div>
 
-		<ul class="our-staff">
+		<ul class="our-staff" id="our-staff">
 			<?php
 				$args = array(
 					'post_type' => 'our_people',
@@ -92,7 +117,7 @@
 		$terms = get_terms( $categories );
 		?>
 		
-	<div class="group-title">	
+	<div class="group-title" >	
 		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-boardmembers.svg"?>">
 		<?php foreach ( $terms as $term ) :  ?>
 			<h3><?php echo $term->name ?></h3>
@@ -100,7 +125,7 @@
 		<?php endforeach; ?>
 	</div>
 
-	<ul class="our-board">
+	<ul class="our-board" id="our-board">
 		<?php
 			$args = array(
 				'post_type' => 'our_people',
@@ -167,7 +192,7 @@
 	</div>
 
 
-	<ul class="our-partners">
+	<ul class="our-partners" id="our-partners">
 		<?php
 			$args = array(
 				'post_type' => 'our_people',
@@ -210,7 +235,7 @@
 		$terms = get_terms( $categories );
 		?>
 		
-	<div class="group-title">	
+	<div class="group-title" >	
 		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-studentbuilding.svg"?>">
 		<?php foreach ( $terms as $term ) :  ?>
 			<h3><?php echo $term->name ?></h3>
@@ -219,7 +244,7 @@
 	</div>
 
 
-	<ul class="our-students">
+	<ul class="our-students" id="our-student">
 		<?php
 			$args = array(
 				'post_type' => 'our_people',
