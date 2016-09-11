87c6f6b8811dded37c15a8332fa13c593c958439
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-10 17:24:15 -0700

    fixed contact content styling top section

--- themes/rise-legal/template-parts/content-contact.php
+++ themes/rise-legal/template-parts/content-contact.php
@@ -14,7 +14,7 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="contact-content">
 		<?php the_content(); ?>
 		<?php
 		wp_link_pages( array(
@@ -23,21 +23,21 @@
 			) );
 			?>
 			<div class="responsive-map">
-			<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.7115811928634!2d-123.11630378412012!3d49.26291238009192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673dd81bd8ebf%3A0x2c3f66039d0db41c!2sRise+Women's+Legal+Centre!5e0!3m2!1sen!2sca!4v1473549430486" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+				<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.7115811928634!2d-123.11630378412012!3d49.26291238009192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673dd81bd8ebf%3A0x2c3f66039d0db41c!2sRise+Women's+Legal+Centre!5e0!3m2!1sen!2sca!4v1473549430486" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 			</div>
 
-			<button>Get Directions</button>
+			<button class="get-directions">Get Directions</button>
 
-			<div class="transit-information">
+			<div class="transit-information contact-notes">
 				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-transit.svg"?>">
 				<?php echo CFS()->get( 'transit_information' ); ?>
 			</div>
-			<div class="parking-information">
+			<div class="parking-information contact-notes">
 				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-noparking.svg"?>">
 				<?php echo CFS()->get( 'parking_information' ); ?>
 			</div>
-			<div class="wait-list-information">
-			<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-hourglass.svg"?>">
+			<div class="wait-list-information contact-notes">
+				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-hourglass.svg"?>">
 				<?php echo CFS()->get( 'wait_list_information' ); ?>
 			</div>
 
