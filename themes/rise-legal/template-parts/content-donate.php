dd10952d44d1c9fd49f0ad59b792f71085c903bd
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-11 15:02:25 -0700

    styling donate page up to justice section

--- themes/rise-legal/template-parts/content-donate.php
+++ themes/rise-legal/template-parts/content-donate.php
@@ -7,63 +7,78 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class=""<?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" class="flex-center donate-container"<?php post_class(); ?>>
 	<header class="entry-header">
 		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-whydonate.svg"?>">
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="donate-content">
 		<?php the_content(); ?>
 		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+			'after'  => '</div>',
 			) );
-		?>
-	<div class="donate-legal-stat">
-		<?php echo CFS()->get( 'legal_stat' ); ?>
-	</div>
+			?>
+			<div class="donate-legal-stat">
+				<?php echo CFS()->get( 'legal_stat' ); ?>
+			</div>
 
-	<div class="donate-criteria">
-		<?php echo CFS()->get( 'criteria' ); ?>
-	</div>
+			<div class="donate-criteria">
+				<?php echo CFS()->get( 'criteria' ); ?>
+			</div>
 
-	<div class="donate-who-title">
-		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>">
-		<h2> who do we help </h2>
-	</div>
+			<div class="donate-who-title">
+				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>">
+				<h2> who do we help </h2>
+			</div>
 
-	<div class="donate-who-content">
-		<?php echo CFS()->get( 'who_do_we_help' ); ?>
-	</div>
+			<div class="donate-who-content">
+				<?php echo CFS()->get( 'who_do_we_help' ); ?>
+			</div>
 
-	<div class="donate-how-title">
-		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-helphand.svg"?>">
-		<h2> how we help </h2>
-	</div>
+			<div class="donate-how-title">
+				<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-helphand.svg"?>">
+				<h2> how we help </h2>
+			</div>
 
-	<div class="donate-how-content">
-		<?php echo CFS()->get( 'how_we_help_main_content' ); ?>
-	</div>
-		
-	<div class="donate-justice-1">
-		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-1.svg"?>">
-		<?php echo CFS()->get( 'how_we_help_justice_1' ); ?>
-	</div>
-	
-	<div class="donate-justice-2">
-		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-2.svg"?>">
-		<?php echo CFS()->get( 'how_we_help_justice_2' ); ?>
-	</div>
+			<div class="donate-how-content">
+				<?php echo CFS()->get( 'how_we_help_main_content' ); ?>
+			</div>
 
-	<div class="donate-justice-3">
-		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-3.svg"?>">
-		<?php echo CFS()->get( 'how_we_help_justice_3' ); ?>
-	</div>
+			<div class="donate-justice-container">
+				<div class="donate-justice">
+					<div class="justice-number-container">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-1.svg"?>">
+					</div>
+					
+					<div class="donate-justice-meta">
+						<?php echo CFS()->get( 'how_we_help_justice_1' ); ?>
+					</div>
+				</div>
+
+				<div class="donate-justice">
+					<div class="justice-number-container">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-2.svg"?>">
+					</div>
+					<div class="donate-justice-meta">
+						<?php echo CFS()->get( 'how_we_help_justice_2' ); ?>
+					</div>
+				</div>
+
+				<div class="donate-justice">
+					<div class="justice-number-container">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-3.svg"?>">
+					</div>
+					<div class="donate-justice-meta">
+						<?php echo CFS()->get( 'how_we_help_justice_3' ); ?>
+					</div>
+				</div>
+			</div>
 
 
 
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+		</div><!-- .entry-content -->
+	</article><!-- #post-## -->
