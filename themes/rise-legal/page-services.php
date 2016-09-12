d74aeec01d1174860e807fd54c53ccfcdf0de9a2
teeppope <terraepop@gmail.com>
2016-09-12 00:12:38 -0700

    get base structure in for who we help section

--- themes/rise-legal/page-services.php
+++ themes/rise-legal/page-services.php
@@ -24,48 +24,45 @@ get_header(); ?>
 						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>" class="rise-about-logo">	
 						<h2>Who We Help</h2>
 					</div>
-					</div>
-					<!-- Staff Listing -->
-					<ul class="services flex-center">
-						<?php
-							$args = array(
-								'post_type' => 'services',
-								'numberposts' => -1,
-								'order' => 'ASC',
-								);
-							$services_page_services_posts = get_posts($args);
-							?>
+					<!-- Services Modal Buttons -->
+					<div>
+						<ul class="services flex-center">
+							<?php
+								$args = array(
+									'post_type' => 'services',
+									'numberposts' => -1,
+									'order' => 'ASC',
+									);
+								$services_page_services_posts = get_posts($args);
+								?>
 
-							<?php foreach($services_page_services_posts as $post) : setup_postdata( $post); ?>
-							<li class="indvid-services">
-								<a href="#<?php the_title();?>" class="services-modal-btn">
-									<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
-										
-								</a>
-								<?php the_title();?>
-								<div id="<?php the_title();?>" class="modalDialog">
-									<div>
-										<a href="#close" title="Close" class="close">
-										</a>
+								<?php foreach($services_page_services_posts as $post) : setup_postdata( $post); ?>
+								<li class="indvid-services">
+									<a href="#<?php the_title();?>" class="services-modal-btn">
 										<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
-										<h2><?php the_title();?></h2>
-										<?php if ( has_post_thumbnail() ) : ?>
-											<div>
-												<?php the_post_thumbnail( 'original' ); ?>
-											</div>
-										<?php endif; ?>
-						
-										<p><?php the_content(); ?></p>
-				
-									</div>
-								</div>
-							</li>
-						<?php endforeach; wp_reset_postdata(); ?>
-
-					</ul>
-				</div>
-
+											
+									</a>
+									<?php the_title();?>
+									<div id="<?php the_title();?>" class="modalDialog">
+										<div>
+											<a href="#close" title="Close" class="close">
+											</a>
+											<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$post->post_name.".svg"?>">
+											<h2><?php the_title();?></h2>
+											<?php if ( has_post_thumbnail() ) : ?>
+												<div>
+													<?php the_post_thumbnail( 'original' ); ?>
+												</div>
+											<?php endif; ?>
+							
+											<p><?php the_content(); ?></p>
 					
+										</div>
+									</div>
+								</li>
+							<?php endforeach; wp_reset_postdata(); ?>
+						</ul>
+					</div>
 				</section>
 
 				<?php get_template_part( 'template-parts/content', 'howtodonate' ); ?>
