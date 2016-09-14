c6772aab2ecfe97f01ad83e5bc0113318b60d3fc
teeppope <terraepop@gmail.com>
2016-09-13 17:30:52 -0700

    get one letter range displaying on resource page
    write comments for next steps

--- themes/rise-legal/page-self-help-center.php
+++ themes/rise-legal/page-self-help-center.php
@@ -31,6 +31,37 @@ get_header(); ?>
 					) ); ?>
 				</ul>
 
+		<!-- Do the GET POSTS stuff for the resources here -->
+
+		<!-- I will need to create a php variable which is an array holding all of my letter ranges -->
+
+		<!-- Then I will need to create a foreach loop of those letter ranges, to then grab the posts associated with them -->
+				<ul>
+				<?php 
+					$args = array(
+						'post_type' => 'resources',
+						'numberposts' => -1,
+						'order' => 'ASC',
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'alpha',
+								'field' => 'slug',
+						//I assume I will replace the below term array with a variable that will hold the letter range.
+								'terms' => array('a','b'),
+								)
+							)
+						);
+					$resources_page_ab_posts = get_posts($args);
+				?>
+				<?php foreach($resources_page_ab_posts as $post) : setup_postdata( $post ); ?>
+					
+					<li>
+						<?php the_content();?>
+					</li>	
+					
+				<?php endforeach; wp_reset_postdata(); ?>
+				</ul>
+
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
