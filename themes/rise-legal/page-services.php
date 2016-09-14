42d5b054e7b94879aa4a9e544033ddb1ed8ecc06
teeppope <terraepop@gmail.com>
2016-09-14 11:23:57 -0700

    edit top of services page content structure

--- themes/rise-legal/page-services.php
+++ themes/rise-legal/page-services.php
@@ -13,10 +13,15 @@ get_header(); ?>
 		
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<section class="about-content">
-					<?php the_title( '<h1 class="front-title">', '</h1>' ); ?>
-					<?php the_content(); ?>
-				</section>
+				<section class="about-content" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+					</div><!-- .entry-content -->
+				</section><!-- #post-## -->
 				
 				<section class="who-we-help">
 					<div class="title-banner services-banner flex-center">
