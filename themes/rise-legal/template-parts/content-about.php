b9a8a6ccf48ecaf9cd4959c06662843666c072e3
teeppope <terraepop@gmail.com>
2016-09-14 11:22:11 -0700

    edit top of about page content structure

--- themes/rise-legal/template-parts/content-about.php
+++ themes/rise-legal/template-parts/content-about.php
@@ -1,7 +1,14 @@
-<section class="about-content">
-	<?php the_title( '<h1 class="front-title">', '</h1>' ); ?>
-	<?php the_content(); ?>
-</section>
+
+<section class="about-content" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</section><!-- #post-## -->
+
 
 <section class="our-people">
 
