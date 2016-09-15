55ffd2fd3aa7328ef25c38338d48026d1adc26bd
teeppope <terraepop@gmail.com>
2016-09-14 22:49:56 -0700

    add spacing structure to contact process on Home

--- themes/rise-legal/front-page.php
+++ themes/rise-legal/front-page.php
@@ -13,7 +13,47 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'front' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="front-banner">
+						
+						<div class="banner-content container">
+							<?php the_title( '<h1 class="front-title">', '</h1>' ); ?>
+							<?php the_content(); ?>
+							<a class="btn" href="<?php echo get_page_link( get_page_by_title( 'About Us' )->ID ); ?>">learn more</a>
+						</div>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+
+						<section class="contact-process container">
+							<h2 class="flex-center"> Contact Process</h2>
+						<!-- This function gets the contact process icons -->
+							<?php get_template_part( 'template-parts/content', 'contact-process' ); ?>
+							<div class="contact-process-content flex-center">
+								<p> 
+									We are now taking appointments for the month of <span class="month"> <?php echo CFS()->get('now_booking_month') ?> </span>
+								</p>
+								<a class="red-btn btn " href="<?php echo get_page_link( get_page_by_title( 'Legal Contact Form' )->ID ); ?>">legal contact form</a>
+								<a class="btn" href="<?php echo get_page_link( get_page_by_title( 'Services' )->ID ); ?>">who can we help</a>
+							</div>
+						</section>
+
+						<section class="donate">
+							<?php get_template_part( 'template-parts/content', 'howtodonate' ); ?>
+							<div class="donate-content flex-center">
+							<a class="btn" href="<?php echo get_page_link( get_page_by_title( 'Why Donate?' )->ID ); ?>">why donate?</a>
+							</div>
+						</section>
+						
+						<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+								'after'  => '</div>',
+							) );
+						?>
+					</div><!-- .entry-content -->
+
+				</article><!-- #post-## -->
 
 			<?php endwhile; // End of the loop. ?>
 
--- themes/rise-legal/template-parts/content-contact-process.php
+++ themes/rise-legal/template-parts/content-contact-process.php
@@ -2,10 +2,4 @@
 	<div class="contact-process-image">
 		<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-contactprocess.svg"?>" width="600px">
 	</div>
-	<div class="contact-process-content">
-		<p class="fill">Fill </br> form</p>
-		<p class="sbmt-form">Submit </br> form</p>
-		<p class="contact-you">We will </br>contact you</p>
-	</div>
-
 </div>
