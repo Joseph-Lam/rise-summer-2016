b7cc1ee80141ab9865e96541cab0219d48da1cc9
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-16 13:53:52 -0700

    rewrote the php file to be more semantic with sections and lists

--- themes/rise-legal/page-why-donate.php
+++ themes/rise-legal/page-why-donate.php
@@ -7,17 +7,99 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'donate' ); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
 
-			<?php endwhile; // End of the loop. ?>
+			<article id="post-<?php the_ID(); ?>" class="flex-center donate-container"<?php post_class(); ?>>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				<section class="about-content container">
+					<header class="entry-header">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-whydonate.svg"?>">
+
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+					<div>
+						<?php the_content(); ?>
+					</div>
+				</section>
+				
+				<section class="donate-content">
+					
+					<div class="title-banner donate-legal-stat">
+						<?php echo CFS()->get( 'legal_stat' ); ?>
+					</div>
+
+					<div class="donate-criteria">
+						<?php echo CFS()->get( 'criteria' ); ?>
+					</div>
+
+					<section>
+						<div class="title-banner donate-who-title">
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>">
+							<h2> who do we help </h2>
+						</div>
+
+						<div class="donate-who-content">
+							<?php echo CFS()->get( 'who_do_we_help' ); ?>
+						</div>
+					</section>
+
+					<section>
+						<div class="title-banner donate-how-title">
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-helphand-white.svg"?>">
+							<h2> how we help </h2>
+						</div>
+
+						<div class="donate-how-content">
+							<?php echo CFS()->get( 'how_we_help_main_content' ); ?>
+						</div>
+					</section>
+
+					<ul class="donate-justice-container">
+						<li class="donate-justice">
+							<div class="justice-number-container">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-1.svg"?>">
+							</div>
+
+							<div class="donate-justice-meta">
+								<?php echo CFS()->get( 'how_we_help_justice_1' ); ?>
+							</div>							
+						</li>
+
+						<li class="donate-justice">
+							<div class="justice-number-container">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-2.svg"?>">
+							</div>
+							<div class="donate-justice-meta">
+								<?php echo CFS()->get( 'how_we_help_justice_2' ); ?>
+							</div>
+						</li>
+
+						<li class="donate-justice">
+							<div class="justice-number-container">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-3.svg"?>">
+							</div>
+							<div class="donate-justice-meta">
+								<?php echo CFS()->get( 'how_we_help_justice_3' ); ?>
+							</div>
+						</li>
+					</ul>
+
+					<section class="donate">
+						<?php get_template_part( 'template-parts/content', 'howtodonate' ); ?>
+					</section>
+
+
+				</section><!-- .donate-content -->
+			</article><!-- #post-## -->
+
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
