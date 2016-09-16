dc7b65278e1a41249d148a55ff112a24d9070a63
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-16 11:55:23 -0700

    added contents-student to page-students and reformated page

--- themes/rise-legal/page-students.php
+++ themes/rise-legal/page-students.php
@@ -8,16 +8,81 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'students' ); ?>
+			<article id="post-<?php the_ID(); ?>" class="flex-center student-container" <?php post_class(); ?>>
 
-			<?php endwhile; // End of the loop. ?>
+				<section class="about-content container">	
+					<header class="entry-header">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						<?php the_title( '<h1 class="student-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+					<div>
+						<?php the_content(); ?>
+					</div>
+
+				</section>
+
+				<section class="students-content">
+
+					<div class="title-banner externship-banner">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-externship-white.svg"?>">
+						<h2> the externship </h2>
+					</div>
+
+					<div class="container student-externship">
+						<?php echo CFS()->get( 'externship' ); ?>
+					</div>
+
+
+					<ul class="container">
+						<li class="student-features">
+							<div class="student-features-icon">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-students-orientation.svg"?>">
+							</div>
+							<div class="student-feature-meta">
+								<?php echo CFS()->get( 'student_orientation' ); ?>
+							</div>
+						</li>
+
+						<li class="student-features">
+							<div class="student-features-icon">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-students-clinic.svg"?>">
+							</div>
+
+							<div class="student-feature-meta">
+								<?php echo CFS()->get( 'legal_clinic' ); ?>
+							</div>
+						</li>
+
+						<li class="student-features">
+							<div class="student-course-icon student-features-icon">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-students-course.svg"?>">
+							</div>
+							<div class="student-course-info student-feature-meta">
+								<?php echo CFS()->get( 'community_lawyering_course' ); ?>
+							</div>
+						</li>
+						
+						<li class="student-features">					<div class="student-reflection-icon student-features-icon">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-students-reflection.svg"?>">
+							</div>
+							<div class="student-reflection-info student-feature-meta">
+								<?php echo CFS()->get( 'reflection_and_rounds' ); ?>
+							</div>
+						</li>
+					</ul>
+
+				</section>
+			</article><!-- #post-## -->
+
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
