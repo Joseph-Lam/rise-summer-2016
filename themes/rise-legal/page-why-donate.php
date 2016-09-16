80a7f153b186d761eecd6e13d6470b2009dec3e4
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-16 14:08:08 -0700

    fixed container styling for top section of donate page

--- themes/rise-legal/page-why-donate.php
+++ themes/rise-legal/page-why-donate.php
@@ -16,7 +16,7 @@ get_header(); ?>
 			<article id="post-<?php the_ID(); ?>" class="flex-center donate-container"<?php post_class(); ?>>
 
 				<section class="about-content container">
-					<header class="entry-header">
+					<header class="entry-header donate-header">
 						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-whydonate.svg"?>">
 
 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
@@ -26,80 +26,80 @@ get_header(); ?>
 					</div>
 				</section>
 				
-				<section class="donate-content">
-					
+				<section>
 					<div class="title-banner donate-legal-stat">
 						<?php echo CFS()->get( 'legal_stat' ); ?>
 					</div>
 
-					<div class="donate-criteria">
+					<div class="donate-criteria container">
 						<?php echo CFS()->get( 'criteria' ); ?>
 					</div>
+				</section>
 
-					<section>
-						<div class="title-banner donate-who-title">
-							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>">
-							<h2> who do we help </h2>
+				<section>
+					<div class="title-banner donate-who-title">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-familyhelp.svg"?>">
+						<h2> who do we help </h2>
+					</div>
+
+					<div class="donate-who-content">
+						<?php echo CFS()->get( 'who_do_we_help' ); ?>
+					</div>
+				</section>
+
+				<section>
+					<div class="title-banner donate-how-title">
+						<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-helphand-white.svg"?>">
+						<h2> how we help </h2>
+					</div>
+
+					<div class="donate-how-content">
+						<?php echo CFS()->get( 'how_we_help_main_content' ); ?>
+					</div>
+				</section>
+
+				<ul class="donate-justice-container">
+					<li class="donate-justice">
+						<div class="justice-number-container">
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-1.svg"?>">
 						</div>
 
-						<div class="donate-who-content">
-							<?php echo CFS()->get( 'who_do_we_help' ); ?>
+						<div class="donate-justice-meta">
+							<?php echo CFS()->get( 'how_we_help_justice_1' ); ?>
+						</div>							
+					</li>
+
+					<li class="donate-justice">
+						<div class="justice-number-container">
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-2.svg"?>">
 						</div>
-					</section>
-
-					<section>
-						<div class="title-banner donate-how-title">
-							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-helphand-white.svg"?>">
-							<h2> how we help </h2>
+						<div class="donate-justice-meta">
+							<?php echo CFS()->get( 'how_we_help_justice_2' ); ?>
 						</div>
+					</li>
 
-						<div class="donate-how-content">
-							<?php echo CFS()->get( 'how_we_help_main_content' ); ?>
+					<li class="donate-justice">
+						<div class="justice-number-container">
+							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-3.svg"?>">
 						</div>
-					</section>
+						<div class="donate-justice-meta">
+							<?php echo CFS()->get( 'how_we_help_justice_3' ); ?>
+						</div>
+					</li>
+				</ul>
 
-					<ul class="donate-justice-container">
-						<li class="donate-justice">
-							<div class="justice-number-container">
-								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-1.svg"?>">
-							</div>
-
-							<div class="donate-justice-meta">
-								<?php echo CFS()->get( 'how_we_help_justice_1' ); ?>
-							</div>							
-						</li>
-
-						<li class="donate-justice">
-							<div class="justice-number-container">
-								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-2.svg"?>">
-							</div>
-							<div class="donate-justice-meta">
-								<?php echo CFS()->get( 'how_we_help_justice_2' ); ?>
-							</div>
-						</li>
-
-						<li class="donate-justice">
-							<div class="justice-number-container">
-								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-3.svg"?>">
-							</div>
-							<div class="donate-justice-meta">
-								<?php echo CFS()->get( 'how_we_help_justice_3' ); ?>
-							</div>
-						</li>
-					</ul>
-
-					<section class="donate">
-						<?php get_template_part( 'template-parts/content', 'howtodonate' ); ?>
-					</section>
+				<section class="donate">
+					<?php get_template_part( 'template-parts/content', 'howtodonate' ); ?>
+				</section>
 
 
-				</section><!-- .donate-content -->
-			</article><!-- #post-## -->
+			</section><!-- .donate-content -->
+		</article><!-- #post-## -->
 
 
-		<?php endwhile; // End of the loop. ?>
+	<?php endwhile; // End of the loop. ?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
