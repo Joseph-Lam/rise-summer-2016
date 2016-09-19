97316bc9cee8dcf4178dd3e2c862fffaa005775a
teeppope <terraepop@gmail.com>
2016-09-19 16:01:21 -0700

    style new staff about section for M/D

--- themes/rise-legal/page-about-us.php
+++ themes/rise-legal/page-about-us.php
@@ -61,48 +61,78 @@ get_header(); ?>
 
 				$groups = get_terms( $categories );
 				?>
-				<div class="about-content flex-center" >	
+				<div class="group-content flex-center " >	
 					<?php foreach ( $groups as $group ) :  ?>
 
 						<!-- GROUP HEADING -->
-						<div class="group-title">
-							<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$group->slug."-dark.svg"?>" class="people-icon">
-							<h3><?php echo $group->name ?></h3>
-							<?php echo $group->description?>
-						</div>
+						<div class="<?php echo $group->slug; ?> container">
+							<div class="group-title">
+								<img src="<?php echo get_template_directory_uri() ."/assets/icons/icon-".$group->slug."-dark.svg"?>" class="people-icon">
+								<h3><?php echo $group->name ?></h3>
+								<?php echo $group->description?>
+							</div>
 
-						<ul class="flex-center people-list our-staff container ">
-							<?php
-							$args = array(
-								'post_type' => 'our_people',
-								'numberposts' => -1,
-								'order' => 'ASC',
-								'tax_query' => array(
-									array(
-										'taxonomy' => 'people_group',
-										'field' => 'slug',
-										'terms' => $group->slug,	
+							<ul class="group-list <?php echo $group->slug; ?>">
+								<?php
+								$args = array(
+									'post_type' => 'our_people',
+									'numberposts' => -1,
+									'order' => 'ASC',
+									'tax_query' => array(
+										array(
+											'taxonomy' => 'people_group',
+											'field' => 'slug',
+											'terms' => $group->slug,	
+											),
 										),
-									),
-								);
-							$about_page_people_posts = get_posts($args);
-							?>
+									);
+								$about_page_people_posts = get_posts($args);
+								?>
 
-							<?php foreach($about_page_people_posts as $post) : setup_postdata( $post); ?>
-								<li class="">
-									<a href="#<?php the_title();?>" class="btn modal-btn">
-										<?php the_title();?>	
-									</a>
+								<?php foreach($about_page_people_posts as $post) : setup_postdata( $post); ?>
+									<li class="group-item">
+										<a href="#<?php the_title();?>" class="btn modal-btn">
+											<?php the_title();?>	
+										</a>
 
-									<div id="<?php the_title();?>" class="modalDialog">
-										<div>
-											<a href="#close" title="Close" class="close">
-											</a>
+										<div id="<?php the_title();?>" class="modalDialog">
+											<div>
+												<a href="#close" title="Close" class="close">
+												</a>
 
-											<h2><?php the_title();?></h2>
+												<h2><?php the_title();?></h2>
 
+												<?php if ( has_post_thumbnail() ) : ?>
+													<div>
+														<?php the_post_thumbnail( 'original' ); ?>
+													</div>
+												<?php endif; ?>
+
+												<?php 
+												$position_title = CFS()->get('position_title');
+
+												if (!empty($position_title)) : ?>
+													<p class="modal-position"><?php echo $position_title?></p>
+												<?php endif; ?>
+
+												<?php 
+												$employed_at = CFS()->get('employed_at'); 
+												if (!empty( $employed_at )) : ?>
+													<p class="modal-employed"><?php echo CFS()->get('employed_at')?></p>
+												<?php endif; ?>
+
+												<?php 
+												$biography = CFS()->get('biography');
+												if (!empty($biography)) : ?>
+													<p class="modal-biography"><?php echo $biography?></p>
+												<?php endif; ?>
+
+											</div>
+										</div>
+
+										<div class="group-item-showing">
 											<?php if ( has_post_thumbnail() ) : ?>
-												<div>
+												<div class="partner-logo">
 													<?php the_post_thumbnail( 'original' ); ?>
 												</div>
 											<?php endif; ?>
@@ -111,64 +141,35 @@ get_header(); ?>
 											$position_title = CFS()->get('position_title');
 
 											if (!empty($position_title)) : ?>
-											<div class="position"><?php echo $position_title?></div>
-										<?php endif; ?>
+												<p class="position"><?php echo $position_title?></p>
+											<?php endif; ?>
 
-										<?php 
-										$employed_at = CFS()->get('employed_at'); 
-										if (!empty( $employed_at )) : ?>
-										<div class="employed"><?php echo CFS()->get('employed_at')?></div>
-									<?php endif; ?>
+											<?php 
+											$employed_at = CFS()->get('employed_at'); 
+											if (!empty( $employed_at )) : ?>
+												<p class="employed"><?php echo CFS()->get('employed_at')?></p>
+											<?php endif; ?>
 
-									<?php 
-									$biography = CFS()->get('biography');
-									if (!empty($biography)) : ?>
-									<div class="biography"><?php echo $biography?></div>
-								<?php endif; ?>
+											<?php 
+											$biography = CFS()->get('biography');
+											if (!empty($biography)) : ?>
+												<div class="biography"><?php echo $biography?></div>
+											<?php endif; ?>
+										</div>
 
-							</div>
+									</li>
+								<?php endforeach; wp_reset_postdata(); ?>
+
+							</ul>
 						</div>
+					<?php endforeach; ?> 
+				</div>
 
-						<div class="">
-							<?php if ( has_post_thumbnail() ) : ?>
-								<div class="partner-logo">
-									<?php the_post_thumbnail( 'original' ); ?>
-								</div>
-							<?php endif; ?>
+			</section>
 
-							<?php 
-							$position_title = CFS()->get('position_title');
+		<?php endwhile; // End of the loop. ?>
 
-							if (!empty($position_title)) : ?>
-							<div class="position"><?php echo $position_title?></div>
-						<?php endif; ?>
-
-						<?php 
-						$employed_at = CFS()->get('employed_at'); 
-						if (!empty( $employed_at )) : ?>
-						<div class="employed"><?php echo CFS()->get('employed_at')?></div>
-					<?php endif; ?>
-
-					<?php 
-					$biography = CFS()->get('biography');
-					if (!empty($biography)) : ?>
-					<div class="biography"><?php echo $biography?></div>
-				<?php endif; ?>
-			</div>
-
-		</li>
-	<?php endforeach; wp_reset_postdata(); ?>
-
-</ul>
-
-<?php endforeach; ?> 
-</div>
-
-</section>
-
-<?php endwhile; // End of the loop. ?>
-
-</main><!-- #main -->
+	</main><!-- #main -->
 </div><!-- #primary -->
 
 
