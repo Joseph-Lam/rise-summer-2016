27837e6a1183822521be3ba91a412b9dc83a6ecf
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-18 19:26:49 -0700

    fixed layout of top page section spacing

--- themes/rise-legal/page-contact.php
+++ themes/rise-legal/page-contact.php
@@ -20,7 +20,9 @@ get_header(); ?>
 				</header><!-- .entry-header -->
 
 				<section class="contact-content">
-					<?php the_content(); ?>
+					<div class="container">
+						<?php the_content(); ?>
+					</div>
 					
 					<div class="responsive-map">
 						<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.7115811928634!2d-123.11630378412012!3d49.26291238009192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673dd81bd8ebf%3A0x2c3f66039d0db41c!2sRise+Women's+Legal+Centre!5e0!3m2!1sen!2sca!4v1473549430486" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
