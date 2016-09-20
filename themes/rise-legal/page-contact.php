c3ef08f278b282a87db377db9ed10c93e7cb1f7e
Joseph-Lam <joseph.lam91@gmail.com>
2016-09-20 14:27:49 -0700

    fixed mobile scrolling lock and styled iframe targeting on dashboard page

--- themes/rise-legal/page-contact.php
+++ themes/rise-legal/page-contact.php
@@ -23,10 +23,6 @@ get_header(); ?>
 					<div class="container">
 						<?php the_content(); ?>
 					</div>
-					
-					<div class="responsive-map">
-						<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.7115811928634!2d-123.11630378412012!3d49.26291238009192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673dd81bd8ebf%3A0x2c3f66039d0db41c!2sRise+Women's+Legal+Centre!5e0!3m2!1sen!2sca!4v1473549430486" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
-					</div>
 
 					<a class="btn" href="https://goo.gl/maps/f1KZEjw458C2">get directions</a>
 
